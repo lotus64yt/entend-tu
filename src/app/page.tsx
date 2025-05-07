0c57d53c208da04bd6e1801d66cffa488ca16e49
lotus64 <lotus64santon@gmail.com>
2025-05-07 13:05:26 +0200

    feat: main page and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,45 @@
-import Image from "next/image";
+import { BgBlueRing } from "@/components/ui/background/BlueRing";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 p-20 font-[family-name:var(--font-geist-sans)]">
+      <BgBlueRing />
+      <div 
+        className=""
+      />
+      <main className="flex flex-col gap-12 pt-16">
+        <h1 className="text-4xl font-bold text-center">
+          M{"'"}entends-tu ? <span className="text-blue-500">Oui</span>, mais
+          parfois <span className="text-red-500">non</span>.
+        </h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <p className="text-zinc-300 text-center">
+          Dans ce site, nous verons les origines du trouble de la surditée et un
+          exemple de personne connue etant atteint par ce trouble.
+        </p>
+
+        <hr className="border-t border-zinc-700 w-full" />
+
+        <article className="flex flex-col gap-4 items-center justify-center bg-zinc-800 p-8 rounded-lg shadow-lg">
+          <h2 className="text-2xl font-bold text-center">
+            Qu&apos;est-ce que la surditée ?
+          </h2>
+          <p className="text-zinc-300 text-center">
+            La surdité est un trouble auditif qui affecte la capacité d&apos;une
+            personne à entendre. Elle peut être causée par divers facteurs,
+            notamment des facteurs génétiques, des infections, des traumatismes
+            acoustiques ou des maladies. La surdité peut être partielle ou
+            totale, et elle peut survenir à tout âge. Les personnes atteintes de
+            surdité peuvent avoir des difficultés à comprendre la parole,
+            surtout dans des environnements bruyants. La surdité peut également
+            affecter la communication et les interactions sociales. Il existe
+            différentes options de traitement et d&apos;adaptation, y compris
+            les appareils auditifs, les implants cochléaires et la langue des
+            signes.
+          </p>
+        </article>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div />
     </div>
   );
 }
