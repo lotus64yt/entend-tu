7420736f5755c6a8999ed92396edb6cda3f60293
lotus64 <lotus64santon@gmail.com>
2025-05-08 18:14:04 +0200

    feat: update padding for layout consistency across pages

--- src/app/personalite/page.tsx
+++ src/app/personalite/page.tsx
@@ -4,7 +4,7 @@ import { FadeIn } from "@/components/ui/animations/FadeIn";
 
 export default function BeethovenPage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 px-4 sm:px-8 md:px-12 lg:px-20 font-[family-name:var(--font-geist-sans)]">
       <BgBlueRing />
       <div className="" />
       <main className="flex flex-col gap-12 pt-12">
