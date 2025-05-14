e784ecc60cfb6d6dc27d5984a9e64f552c169dc0
lotus64 <lotus64santon@gmail.com>
2025-05-14 12:50:41 +0200

    refractor: layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen text-white`}
       >
