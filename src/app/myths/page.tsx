69337cb6d691ca4ff7968d041c156cbb6aed7081
lotus64 <114228798+lotus64yt@users.noreply.github.com>
2025-05-14 12:42:19 +0200

    feat: change myth page bg

--- src/app/myths/page.tsx
+++ src/app/myths/page.tsx
@@ -26,7 +26,7 @@ export default function MythsPage() {
   ];
 
   return (
-    <div className="relative isolate overflow-hidden bg-white dark:bg-gray-900 px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
+    <div className="relative isolate overflow-hidden px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0">
       <div className="absolute inset-0 -z-10 overflow-hidden">
         <BgBlueRing />
       </div>
